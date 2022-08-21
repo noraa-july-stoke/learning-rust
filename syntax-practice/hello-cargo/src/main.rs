6ac0d61a39432f654433a6bfde2bd8c6533b450c
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-08-20 21:03:41 -0400

    add guessing-game

--- syntax-practice/hello-cargo/src/main.rs
+++ syntax-practice/hello-cargo/src/main.rs
@@ -6,3 +6,6 @@ fn main() {
 // cargo build --> compiles & tracks dependencies
 // executable file is found within target/debug
 // cargo run --> compiles & executes in one command
+// cargo new --> create cargo project
+// cargo cehck --> build without creating executable
+// cargo build --release creates a 'release' build
---
+++ syntax-practice/hello-cargo/target/debug/deps/hello_cargo-f4760cbb08d3cb3c.d
@@ -0,0 +1,5 @@
+/Users/mark.stoke/software-dev/rust/syntax-practice/hello-cargo/target/debug/deps/hello_cargo-f4760cbb08d3cb3c.rmeta: src/main.rs
+
+/Users/mark.stoke/software-dev/rust/syntax-practice/hello-cargo/target/debug/deps/hello_cargo-f4760cbb08d3cb3c.d: src/main.rs
+
+src/main.rs:
